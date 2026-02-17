3a809a410ef3df9ff103b404e437d9cf605827ae
Pablocheee <pablo.che.sunset@gmail.com>
2026-02-17 04:48:45 +0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,14 @@
-import './24a9545adcfd6bb1.css';
-import './6809280247a39ba0.css';
-import './globals.css'; // Оставь, если этот файл есть в папке app
+import './style1.css';
+import './style2.css';
+import './globals.css'; 
 
 export const metadata = {
   title: 'AIO.CORE',
-  description: 'Semantic Indexing Protocol',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="ru">
-      <head>
-        <link rel="icon" href="/favicon.ico" />
-      </head>
       <body className="antialiased">
         {children}
       </body>
